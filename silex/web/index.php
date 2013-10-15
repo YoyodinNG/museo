e82fc1e1135a8fa3b9e2d7e3e1741c9b5d1af993
YoyodinNG <javnue@gmail.com>
2013-10-15 13:19:53 +0200

    #Probando Git 2, desde SmartGit

--- silex/web/index.php
+++ silex/web/index.php
@@ -4,7 +4,7 @@ require_once __DIR__.'/../vendor/autoload.php';
 
 $app = new Silex\Application();
 
-//pequeño cambio, probando Git...
+//pequeño cambio, probando Git...#2
 $app->get('/hello', function() {
     return 'Hello World!';
 });
